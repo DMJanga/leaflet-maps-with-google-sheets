c997e4d80a603f07503b030733c33dfe357d8a5f
DMJanga <dmpereira2002@gmail.com>
2017-03-26 19:22:07 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hP1ejZYNBrRMlfIsmjEkCIzz3jaTBr4lyOfeNIidI2U/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1hP1ejZYNBrRMlfIsmjEkCIzz3jaTBr4lyOfeNIidI2U/pubhtml';
